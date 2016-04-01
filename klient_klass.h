7df6e28bdfb0c51eea3424bda50cea0951e06656
bufu <laurts@hotmail.com>
2016-04-01 15:33:12 +0000

    klient klass seisuga 01.04

---
+++ klient_klass.h
@@ -0,0 +1,28 @@
+#ifndef KLIENT_H
+#define KLIENT_H
+#include <iostream>
+#include <string>
+
+class Klient{
+  private:
+    std::string kEesnimi;
+    std::string kPerenimi;
+    std::string kKliendi_id;
+    int kTelefon;
+    std::string kEmail;
+
+    Klient(){};
+    
+  public:
+    Klient(std::string Eesnimi, std::string Perenimi, std::string Kliendi_id, int Telefon, std::string Email){
+        kEesnimi = Eesnimi;
+        kPerenimi = Perenimi;
+        kKliendi_id = Kliendi_id;
+        kTelefon = Telefon;
+        kEmail = Email;
+        
+    }      
+  
+};
+
+#endif
