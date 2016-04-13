39c596202bcbcc24da0efb608a6dd301ec7366dc
efka87 <evelin.pindmaa@gmail.com>
2016-04-14 00:12:20 +0300

    Added files via upload

---
+++ autod.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<cstdlib>
+
+using namespace std;
+int main()
+{
+int valik;
+
+cout<< "Palun vali soiduki tüüp (soiduauto/maastur/pakibuss)" << "" << endl;
+cin>> valik;
+
+return 0;
+}
