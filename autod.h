47177b857705150dcfdc3026b15dc31f88f69670
donjohnsmith <erkko.ortus@gmail.com>
2016-05-16 06:10:40 +0000

    added functionality

--- autod.h
+++ autod.h
@@ -9,15 +9,16 @@ class Autod{
     std::string aMudel;
     int aAasta;
     std::string aKytus;
-    int aMootor;
+    float aMootor;
     std::string aKaigukast;
     std::string aVeoskeem;
     std::string aVIN;
+    int aID;
                                      
 
     Autod(){};
   public:
-    Autod(std::string mark, std::string mudel, int aasta, std::string kytus, int mootor, std::string kaigukast, std::string veoskeem, std::string vin){
+    Autod(std::string mark, std::string mudel, int aasta, std::string kytus, float mootor, std::string kaigukast, std::string veoskeem, std::string vin, int autoID){
         aMark = mark;
         aMudel = mudel;                     //konstruktor andmete sisestamiseks,  ei ole kindel vb peab seda veel muutma
         aAasta = aasta;
@@ -26,11 +27,40 @@ class Autod{
         aKaigukast = kaigukast;
         aVeoskeem = veoskeem;
         aVIN = vin;
+        aID = autoID;
     }
 
-  //  void getInfo(){
-    //    std::cout << "Name: " << aName << " : " << aNumber << std::endl;
-
+    void getInfo(){
+        std::cout <<"Auto: "<<aMark << " : " << aMudel<< " : " << aAasta<< " : " << aKytus<< " : " << aMootor<< " : " << aKaigukast<< " : " << aVeoskeem<< " : " << aVIN<< " : " << aID<<std::endl;
+    }
+    
+    std::string getmark(){
+      return aMark;
+    }
+    std::string getmudel(){
+      return aMudel;
+    } 
+    int getaasta(){
+      return aAasta;
+    }    
+    std::string getkytus(){
+      return aKytus;
+    }
+    float getmootor(){
+      return aMootor;
+    } 
+    std::string getkaigukast(){
+      return aKaigukast;
+    }    
+    std::string getveoskeem(){
+      return aVeoskeem;
+    }
+    std::string getvin(){
+      return aVIN;
+    }   
+    int getautoID(){
+      return aID;
+    }
   
 };
 
