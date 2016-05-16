935bcbb6cada684c8a647506e61cde71407c6eed
donjohnsmith <erkko.ortus@gmail.com>
2016-05-16 06:40:32 +0000

    kokku klopsimine

--- main.cpp
+++ main.cpp
@@ -8,6 +8,66 @@
 
 using namespace std;
 
+
+vector<Klient*> klientLisamine(string Eesnimi, string Perenimi, int Kliendi_id, string Telefon, string Email, vector<Klient*> klientNimekiri){
+    cout << endl << "Enter kliendi Eesnimi: ";
+    getline(cin,Eesnimi);  //getline lisamisel lahenes tühiku kasutamise probleem aga tekkis failist lugemise error(failis pole andmetüüpe, loeb tühikust tühikuni)
+    cout << endl << "Enter kliendi Perenimi: ";
+    getline(cin,Perenimi);
+    cout << endl << "Enter kliendi Kliendi_id: ";
+    cin >> Kliendi_id;
+    cout << endl << "Enter kliendi Telefon: ";
+    cin >> Telefon;
+    cout << endl << "Enter kliendi Email: ";
+    cin >> Email;
+    
+    Klient *newKlient = new Klient(Eesnimi, Perenimi, Kliendi_id, Telefon, Email);
+    klientNimekiri.push_back(newKlient);
+    cout << "New client entered successfully" << endl;
+    
+    return klientNimekiri;
+}
+
+// klientide failist lugemine
+
+vector<Klient*> nimekirjaFailistLugemine(string Eesnimi, string Perenimi,int Kliendi_id, string Telefon, string Email, vector<Klient*> klientNimekiri){
+    
+    ifstream iFile;
+    iFile.open("klient.txt");
+    
+    while(iFile.good()){
+        
+        iFile >> Eesnimi >> Perenimi >> Kliendi_id >> Telefon >> Email;                  // loen failist kliendi andmed
+        Klient *newKlient = new Klient(Eesnimi, Perenimi, Kliendi_id, Telefon, Email);   //kliendi andmed newKlient viita
+        klientNimekiri.push_back(newKlient);                   // lükkan kliendi newKlient klientNimekirja
+        
+    }
+    klientNimekiri.pop_back();
+    
+    iFile.close();
+    
+    return klientNimekiri;
+}
+
+
+void andmeteFailiSalvestamine(vector<Klient*> klientNimekiri){
+    
+    ofstream oFile;
+    oFile.open("klient.txt");
+    
+    for(int i=0; i<klientNimekiri.size();i++){
+        oFile << klientNimekiri[i]->getEesnimi() 
+            << " " << klientNimekiri[i]->getPerenimi()
+            << " " << klientNimekiri[i]->getKliendi_id() 
+            << " " << klientNimekiri[i]->getTelefon()
+            << " " << klientNimekiri[i]->getEmail()
+            << endl;
+    }
+    
+    cout << "Edukalt salvestatud" << endl;
+    oFile.close();
+}
+
 int getID( vector<Autod*> autoNimekiri){
     int u,v,ID;
     // kui autoNimekisi on tühi siis esimene auto saab id-ks 0
@@ -216,6 +276,10 @@ int main(){
     float mootor;
     int a[5][8];
     int rida,veerg;
+     // muutujate deklareerimine
+    vector<Klient*> klientNimekiri;
+    string Eesnimi, Perenimi, Telefon, Email;
+    int Kliendi_id;
     
     // loeb autod failist nimekirja
     autoNimekiri=nimekirjaFailistLugemine(mark,mudel,aasta, kytus, mootor, kaigukast, veoskeem,  vin, autoID, autoNimekiri);
@@ -223,6 +287,13 @@ int main(){
     autoNimekiri=autoLisamine(mark,mudel,aasta, kytus, mootor, kaigukast, veoskeem,  vin, autoID, autoNimekiri);
     // salvestab autonimekirja faili
     nimekirjaFailiSalvestamine(autoNimekiri);
+    
+     // loeb klient failist nimekirja
+    klientNimekiri=nimekirjaFailistLugemine(Eesnimi, Perenimi, Kliendi_id, Telefon, Email, klientNimekiri);
+    // kliendi käsitsi nimekirja lisamine
+    klientNimekiri=klientLisamine(Eesnimi, Perenimi, Kliendi_id, Telefon, Email, klientNimekiri);
+    // salvestab klient nimekirja faili
+    andmeteFailiSalvestamine(klientNimekiri);
 
     // väljastab autoNimekiri sisu
     for(int i = 0;i<autoNimekiri.size();i++){
